45eb4f6e573b700e960440a0662cf39ef599cd92
Meherose Hossain Tasnia <mhtasnia@gmail.com>
2025-02-15 13:12:56 +0600

    Added Contribution and registration pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Homepage'
+import Login from './pages/Login'
+import Footer from './Components/Footer'
+import Signup from './pages/Signup'
+import Contributions from './pages/Contributions'
 
 function App() {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/contributions" element={<Contributions />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
