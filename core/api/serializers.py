939a9cc4b09f2c859394e836afa5e81474ed95ff
Jahidul Hassan Reshad <reshad22205341197@diu.edu.bd>
2025-02-21 22:33:26 +0600

    hidden the secret keys

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,16 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import User_Profile
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Profile
+        fields = ['id', 'user', 'profile_picture', 'is_email_verified', 'is_profile_verified', 'is_active', 'created_at', 'updated_at', 'date_of_birth', 'university', 'department', 'major_subject']
+
+
--- core/core/settings.py
+++ core/core/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,12 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'  # Convert string to boolean
 
 ALLOWED_HOSTS = ['*']
 
@@ -48,6 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'core',
     'auth_app',
+    'api',
 ]
 
 MIDDLEWARE = [
